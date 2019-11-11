9b9e2b08b851990b01a55613136ae128fdc1fcb7
João Pedro <joaopedro453@hotmail.com>
2019-11-11 20:11:36 -0300

    Ajax com jQuery - Requisições para o back-end com load(), get() e post()

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 $(document).ready(() => {
-	
+
+    $('#documentacao-btn').click(function (e) {
+        // $('#pagina').load('documentacao.html')
+        $.get('documentacao.html', data => {
+            $('#pagina').html(data)
+        })
+    });
+
+    $('#suporte-btn').click(function (e) {
+        // $('#pagina').load('suporte.html')
+        $.post('suporte.html', data => {
+            $('#pagina').html(data)
+        })
+    });
+
+
 })
